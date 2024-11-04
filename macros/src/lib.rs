fb1e1c52c04b0b1b5a345b2ed7bc4a60c528cbf5
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2024-11-03 22:00:08 -0500

    fix: derive type aliases

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,4 +1,5 @@
 use proc_macro::TokenStream;
+use quote::format_ident;
 
 /// Derive macro for adding convenient event constructors to an audio channel.
 #[proc_macro_derive(AudioChannel)]
@@ -6,7 +7,17 @@ pub fn derive_channel(input: proc_macro::TokenStream) -> proc_macro::TokenStream
     let ast: syn::DeriveInput = syn::parse(input).unwrap();
     let name = &ast.ident;
 
+    let play_type_name = format_ident!("{}PlayEvent", name);
+    let track_type_name = format_ident!("{}TrackEvent", name);
+    let volume_type_name = format_ident!("{}VolumeEvent", name);
+    let default_type_name = format_ident!("{}DefaultSettingsEvent", name);
+
     let expanded = quote::quote! {
+        pub type #play_type_name = PlayEvent<#name>;
+        pub type #track_type_name = TrackEvent<#name>;
+        pub type #volume_type_name = VolumeEvent<#name>;
+        pub type #default_type_name = DefaultSettingsEvent<#name>;
+
         impl #name {
             pub fn new_play_event(id: AudioFiles) -> PlayEvent<#name> {
                 PlayEvent::new(id)
@@ -16,6 +27,9 @@ pub fn derive_channel(input: proc_macro::TokenStream) -> proc_macro::TokenStream
             }
             pub fn new_volume_event(volume: f32) -> VolumeEvent<#name> {
                 VolumeEvent::new(volume)
+            }
+            pub fn new_default_settings_event(settings: PlaybackSettings) -> DefaultSettingsEvent<#name> {
+                DefaultSettingsEvent::new(settings)
             }
         }
     };
